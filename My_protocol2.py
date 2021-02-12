7092bcc9654df39a11d743ef8b0e97921467e011
LauraMarras <78542448+LauraMarras@users.noreply.github.com>
2021-02-12 11:54:05 +0100

    add instructions

--- My_protocol2.py
+++ My_protocol2.py
@@ -74,8 +74,19 @@ class DecisionMakingGui:
     stim_path = ''
     coin_sound = (r'Sound Stimuli/coins.mp3')
     buzz_sound = (r'Sound Stimuli/buzz.mp3')
-
-
+    instructions = '''
+    Task Instructions:
+    During this task, a total of 30 stimuli will be presented. There will be 5 runs of 6 stimuli each, where each stimulus is repeated 4 times in randomized order.
+    Half of the stimuli will be “winning” stimuli, the other half will be “losing” stimuli.
+    Your goal is to choose, each time a stimulus is presented, whether it belongs to the winning or losing category.
+    Note that each stimulus is randomly assigned to one of the categories, so you will have to guess them at first, and learn trial by trial, there is no hidden logic behind the associations.
+    
+    1. At the beginning of each trial, you will see a cross in the middle of the screen, please try to maintain central fixation throughout the trial.
+    2. Then, a stimulus will be presented, you should fixate the stimulus and withhold any response until the stimulus disappears.
+    3. At stimulus offset, you will be asked to press either the W or L key on the keyboard, indicating whether the stimulus belongs to the Winning or Losing category. You will have 1 second to press the key, after which the time will expire and the trial will be lost, so try to choose as soon as possible.
+    4. After you press the key (if you did it on time), you will get a feedback indicating whether you did the correct choice or not.
+    5. Right after a new trial will begin.
+    '''
 
     def __init__(self, root, my_set):
         self.root = root
@@ -100,7 +111,7 @@ class DecisionMakingGui:
         self.label = tk.Label(font=('Helvetica bold', 30), bg='black', fg='white')
         self.lblVar = tk.StringVar()
         self.label.configure(textvariable=self.lblVar)
-        self.lblVar.set('Press <spacebar> to start')
+        self.lblVar.set(self.instructions + '\n Press <spacebar> to start')
         self.label.pack(expand=1)
 
         #Press spacebar to start
