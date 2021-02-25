91915ca4ea29d99c88b00a5fcfa103411a693416
LauraMarras <78542448+LauraMarras@users.noreply.github.com>
2021-02-25 19:28:27 +0100

    implementing analysis
    not finished yet

--- ReadStream.py
+++ ReadStream.py
@@ -1,6 +1,4 @@
 import pyxdf
-import matplotlib.pyplot as plt
-import numpy as np
 
 data, header = pyxdf.load_xdf(r'block_Default.xdf')
 
@@ -10,12 +8,8 @@ for stream in data:
     if isinstance(y, list):
         # list of strings, draw one vertical line for each marker
         for timestamp, marker in zip(stream['time_stamps'], y):
-            plt.axvline(x=timestamp)
-            print(f'Marker "{marker[0]}" @ {timestamp:.2f}s')
-    # elif isinstance(y, np.ndarray):
-    #     # numeric data, draw as lines
-    #     plt.plot(stream['time_stamps'], y)
+            print(f'Marker "{marker[0]}" @ {timestamp:.4f} s')
+            if (marker[0])
     else:
         raise RuntimeError('Unknown stream format')
-
-plt.show()
+    
